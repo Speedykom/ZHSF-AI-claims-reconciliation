a897be65dd5b330075623731f2979ef5e27abb18
Med Dhia Kassab <dhia.kassab@speedykom.de>
2025-11-28 17:38:10 +0100

    refactor database query execution functions

--- claims-rec-mcp/db.py
+++ claims-rec-mcp/db.py
@@ -10,12 +10,15 @@ db_port = os.getenv("TEST_DB_PORT", "5432")
 conn = psycopg.connect(f"dbname={db_name} user={db_user} password={db_password} host={db_host} port={db_port}")
 cur = conn.cursor()
 
-# function to execute a query
 def execute_query(query: str):
     cur.execute(query)
     return cur.fetchall()
 
-# function to close the connection
+def execute_update(query: str):
+    cur.execute(query)
+    conn.commit()
+    return cur.rowcount
+
 def close_connection():
     cur.close()
     conn.close()
